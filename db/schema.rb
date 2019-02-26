c632384985b2d35ebb88d73ce3b1b82869eb4a9c
Janie Amero <janie.amero@gmail.com>
2019-02-26 19:50:54 +0000

    added fields to booking and updated booking new form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_140843) do
+ActiveRecord::Schema.define(version: 2019_02_26_193949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_02_26_140843) do
     t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "message"
+    t.datetime "time"
     t.index ["task_id"], name: "index_bookings_on_task_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
