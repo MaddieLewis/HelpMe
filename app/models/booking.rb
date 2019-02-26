f13729112b32d1247a3738728ccb9be4e40a2ffb
Janie Amero <janie.amero@gmail.com>
2019-02-26 14:17:59 +0000

    changed confirmed to staatus in the booking model and added an inclusion validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :task
+  validates :status, inclusion: { in: ["pending", "confirmed", "declined", "cancelled"] }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_170639) do
+ActiveRecord::Schema.define(version: 2019_02_26_140843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_170639) do
   create_table "bookings", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "task_id"
-    t.boolean "confirmed", default: false
+    t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["task_id"], name: "index_bookings_on_task_id"
