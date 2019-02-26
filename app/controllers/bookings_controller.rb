5d6e4782fe80e74e5ff9009d9489d87044ea93a9
alexandra campbell <killeraliens@outlook.com>
2019-02-26 12:17:19 +0000

    booking policy index controller  and view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,7 @@ class BookingsController < ApplicationController
   # index
   def index
     # need to only show pending or confirmed booking belonging to the user that are yet to happen
-    @bookings = Booking.where(user: current_user)
+    @bookings = policy_scope(Booking)
   end
 
   # show
