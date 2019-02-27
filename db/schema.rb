a0f1c249e1fd59b6563101fe3def6e4df7f252b3
Mark Gould <m.gould16@gmail.com>
2019-02-27 11:29:42 +0000

    nav bar with redirects working

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2019_02_26_195201) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "chats", force: :cascade do |t|
+    t.text "message"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.bigint "user_id"
     t.string "title"
