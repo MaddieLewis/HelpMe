0ce7055b970bfbc5aff1af9b554ce917c03c7dfb
alexandra campbell <killeraliens@outlook.com>
2019-02-28 17:24:43 +0000

    sort by date, show only upcoming tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,9 +6,12 @@ class TasksController < ApplicationController
   def index
     # @tasks = Task.all
     if params[:query].present?
-      @tasks = Task.search_by_task_title_category_description(params[:query]).where.not(latitude: nil, longitude: nil)
+      @tasks = Task.search_by_task_title_category_description(params[:query])
+      .where.not(latitude: nil, longitude: nil).order('start_time')
+      @tasks = @tasks.select { |task| task.end_time - Time.now > 0 }
     else
-      @tasks = Task.where.not(latitude: nil, longitude: nil)
+      @tasks = Task.where.not(latitude: nil, longitude: nil).order('start_time')
+      @tasks = @tasks.select { |task| task.end_time - Time.now > 0 }
     end
 
     @markers = @tasks.map do |task|
@@ -24,15 +27,6 @@ class TasksController < ApplicationController
   def show
     @user = @task.user
     @booking = Booking.new
-    array = [@task]
-    @markers = array.map do |task|
-      {
-        lng: task.longitude,
-        lat: task.latitude,
-        infoWindow: render_to_string(partial: "showinfowindow", locals: { task: @task }),
-        image_url: helpers.asset_url('hand_icon.png')
-      }
-    end
   end
 
   def new
