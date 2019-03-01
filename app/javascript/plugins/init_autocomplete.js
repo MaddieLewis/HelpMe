c5fa3543a333f3f1f6ac19cb9583b2eadebdf334
Janie Amero <janie.amero@gmail.com>
2019-03-01 14:05:35 +0000

    got algolia to work on front page search

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,10 +1,12 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.getElementById('task_address');
-  if (addressInput) {
-    places({ container: addressInput });
+  const addressInput = document.querySelectorAll('#task_address, #address');
+  addressInput.forEach(element => {
+    if (element) {
+    places({ container: element });
   }
+  });
 };
 
 export { initAutocomplete };
